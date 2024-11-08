c6ec303cc807c7eff9d8b39cda37efaae81f1d66
Rakibul Islam <sheikh2rin@gmail.com>
2024-11-07 22:55:29 -0700

    Merge with Dev (#1)
    * installed adminlte
    
    * installed fortify
    
    * added login, register and forgot password pages
    
    * users can login and logout

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('home', function() {
+    return "Home";
+})->name('home');
+
+Route::group(['prefix' => 'dashboard', 'as' => 'dashboard.', 'middleware' => 'auth'], function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('index');
 });
