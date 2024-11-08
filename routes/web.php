20282abe5350b58b614794dd9f8aaba3daa51651
Rakibul Islam <sheikh2rin@gmail.com>
2024-11-08 00:52:46 -0700

    added route for country, state and city

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\CityController;
+use App\Http\Controllers\CountryController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\StateController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,4 +16,8 @@ Route::get('home', function() {
 
 Route::group(['prefix' => 'dashboard', 'as' => 'dashboard.', 'middleware' => 'auth'], function () {
     Route::get('/', [DashboardController::class, 'index'])->name('index');
+    
+    Route::resource('country', CountryController::class);
+    Route::resource('state', StateController::class);
+    Route::resource('city', CityController::class);
 });
